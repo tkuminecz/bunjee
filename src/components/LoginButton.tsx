39d6d15fb78cc303998695bf140b4811df7d08a3
Tim Kuminecz <tkuminecz@gmail.com>
2022-11-23 02:14:00 -0500

    couple tweaks

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import { signIn } from 'next-auth/react'
 
-const LoginButton: React.FC = () => {
-  return (
-    <button onClick={() => signIn()}>
-      <a>Login</a>
-    </button>
-  )
+const LoginButton = () => {
+  return <button onClick={() => signIn()}>Login</button>
 }
 
 export default LoginButton
